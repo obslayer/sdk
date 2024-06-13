df16c31ea52f82eea71bf7353c3dddbda32a850e
wangyicong661 <wangyicong661@hellobike.com>
2024-06-13 17:24:51 +0800

    change test url

--- python/tests/bluepipe_test.py
+++ python/tests/bluepipe_test.py
@@ -4,15 +4,19 @@ from lib import bluepipe
 
 
 class BluePipeTest(unittest.TestCase):
-    client = bluepipe.BluePipe('http://127.0.0.1/api/v1////', 'etl', 'yZz6XTYSaiyx5q2u')
+    client = bluepipe.BluePipe('http://demo.1stblue.cloud/api/v1////', 'etl', 'yZz6XTYSaiyx5q2u')
 
     def test_search_lineage(self):
         result = self.client.search_lineage('i.am.not.exist')
         self.assertListEqual(result, [])
 
-        result = self.client.search_lineage('tpch.lineitem')
+        # result = self.client.search_lineage('tpch.lineitem')
+        result = self.client.search_lineage('t')
         self.assertTrue(len(result) > 0)
 
         first = result[0]
         self.assertIsNotNone(first.get('dst_table'))
+        print(first.get('dst_table'))
         self.assertIsNotNone(first.get('job_guid'))
+        print(first.get('job_guid'))
+        print(first)
